e736c6c621e29ce7cfac1d484eb95b61e5cddd29
AlejandroGrellet <mbmnubia@gmail.com>
2024-06-09 22:45:16 -0300

    tp3-added new audit table for LINEAS_PARTES

--- LBD2024G06TP3Script.sql
+++ LBD2024G06TP3Script.sql
@@ -2,20 +2,35 @@ USE `lbd2024g06agrosa`;
 DROP TABLE IF EXISTS `AUDITORIAS_PARTES`;
 CREATE TABLE IF NOT EXISTS `AUDITORIAS_PARTES`
 (
-	`idAUDITORIA` 		  INT 		  NOT NULL AUTO_INCREMENT,
+	`idAUDITORIA_PARTE`   INT 		  NOT NULL AUTO_INCREMENT,
     `usuario` 	  		  VARCHAR(30) NOT NULL,
     `host` 		  		  VARCHAR(30) NOT NULL,
+    `tipoOperacion`		  ENUM ('Incersion', 'Modificacion', 'Borrado') NOT NULL,
     `fecha` 	  		  DATETIME 	  NOT NULL,
+    `idPARTE`			  INT		  NOT NULL,
+    `idFINCA`			  INT		  NOT NULL,
     `nombreFinca` 		  VARCHAR(45) NOT NULL,
     `fechaParte`  		  DATE        NOT NULL,
     `estadoParte`  		  CHAR(1)     NOT NULL,
     `superficie`  		  FLOAT(11)   NOT NULL,
+    `idENCARGADO`		  INT 		  NOT NULL,
     `nombresEncargado` 	  VARCHAR(30) NOT NULL,
-    `apellidosEncargado`  VARCHAR(30) NOT NULL,
-    `nombresEmpleado`     VARCHAR(30) NOT NULL,
-    `apellidosEmpleado`   VARCHAR(30) NOT NULL,
-    `rol`         		  CHAR(1) 	  NULL DEFAULT NULL
-    PRIMARY KEY(`idAUDITORIA`)
+    `apellidosEncargado`  VARCHAR(30) NOT NULL
+    PRIMARY KEY(`idAUDITORIA_PARTE`)
+);
+DROP TABLE IF EXISTS `AUDITORIAS_LINEAS_PARTES`;
+CREATE TABLE IF NOT EXISTS `AUDITORIAS_LINEAS_PARTES`
+(
+	`idAUDITORIA_LINEA_PARTE`   INT 		NOT NULL AUTO_INCREMENT,
+	`usuario` 	  		  		VARCHAR(30) NOT NULL,
+    `host` 		  		  		VARCHAR(30) NOT NULL,
+    `tipoOperacion`		  		ENUM ('Incersion', 'Modificacion', 'Borrado') NOT NULL,
+    `idPARTE`			 		INT		    NOT NULL,
+    `idEMPLEADO`			 	INT		    NOT NULL,
+    `nombresEmpleado`     		VARCHAR(30) NOT NULL,
+    `apellidosEmpleado`   		VARCHAR(30) NOT NULL,
+    `rol`         		  		CHAR(1) 	NULL DEFAULT NULL
+    
 );
 
 DELIMITER $$
