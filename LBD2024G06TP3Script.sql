f3cbbd8df9316c54fe9307acf888f5f3c6b8f544
AlejandroGrellet <mbmnubia@gmail.com>
2024-06-11 09:23:27 -0300

    Finished my side of the assignment with calls and positive tests :)

--- LBD2024G06TP3Script.sql
+++ LBD2024G06TP3Script.sql
@@ -37,7 +37,6 @@ DROP TRIGGER IF EXISTS `creacionPartesTrigg`;
 DROP TRIGGER IF EXISTS `modificacionPartesTrigg`;
 
 DELIMITER $$
-
 CREATE TRIGGER `creacionPartesTrigg` 
 AFTER INSERT ON `PARTES`
 FOR EACH ROW
@@ -45,8 +44,9 @@ BEGIN
 	DECLARE username 	VARCHAR(30);
     DECLARE hostname 	VARCHAR(30); 
     
-    SET username = CURRENT_USER();
-    SELECT @hostname INTO hostname;
+    SET username = SUBSTRING_INDEX(USER(), '@', 1);
+    SET hostname = SUBSTRING_INDEX(USER(), '@', -1);
+    
     INSERT INTO AUDITORIAS_PARTES VALUES(
 		DEFAULT,
         username,
@@ -69,8 +69,9 @@ BEGIN
 	DECLARE username 	VARCHAR(30);
     DECLARE hostname 	VARCHAR(30); 
     
-    SET username = CURRENT_USER();
-    SELECT @hostname INTO hostname;
+    SET username = SUBSTRING_INDEX(USER(), '@', 1);
+    SET hostname = SUBSTRING_INDEX(USER(), '@', -1);
+    
     INSERT INTO AUDITORIAS_PARTES VALUES(
 		DEFAULT,
         username,
@@ -98,7 +99,6 @@ BEGIN
         NEW.superficie
     );
 END $$
-
 DELIMITER ;
 
 -- Triggers para la tabla LINEAS_PARTES
@@ -106,7 +106,6 @@ DROP TRIGGER IF EXISTS `creacionLineasPartesTrigg`;
 DROP TRIGGER IF EXISTS `modificacionLineasPartesTrigg`;
 
 DELIMITER $$
-
 CREATE TRIGGER `creacionLineasPartesTrigg` 
 AFTER INSERT ON `LINEAS_PARTES`
 FOR EACH ROW
@@ -114,8 +113,9 @@ BEGIN
 	DECLARE username 	VARCHAR(30);
     DECLARE hostname 	VARCHAR(30); 
     
-    SET username = CURRENT_USER();
-    SELECT @hostname INTO hostname;
+    SET username = SUBSTRING_INDEX(USER(), '@', 1);
+    SET hostname = SUBSTRING_INDEX(USER(), '@', -1);
+    
     INSERT INTO AUDITORIAS_LINEAS_PARTES VALUES(
 		DEFAULT,
         username,
@@ -135,8 +135,9 @@ BEGIN
 	DECLARE username 	VARCHAR(30);
     DECLARE hostname 	VARCHAR(30); 
     
-    SET username = CURRENT_USER();
-    SELECT @hostname INTO hostname;
+    SET username = SUBSTRING_INDEX(USER(), '@', 1);
+    SET hostname = SUBSTRING_INDEX(USER(), '@', -1);
+    
     INSERT INTO AUDITORIAS_LINEAS_PARTES VALUES(
 		DEFAULT,
         username,
@@ -158,91 +159,263 @@ BEGIN
         NEW.rol
     );
 END $$
-
 DELIMITER ;
 
 -- SECCION PARA LOS STORE PROCEDURES
-DELIMITER $$
+
+
 -- Punto 4: Creación de un Parte y sus Líneas.
--- Consideraciones: Se recibe un request con los siguientes datos:
--- 		Nombre de la finca
--- 		Cuil del encargado
--- 		Nombre del empleado que oficia como encargado
--- 		Apellido del empleado que oficia como encargado
--- 		Fecha
--- 		Superficie
--- 		Cuil del empleado
--- 		Nombre del empleado 
--- 		Apellido del empleado
--- 		Rol del empleado 
-CREATE PROCEDURE `PUNTO 4` (IN nombreFinca VARCHAR(30), fechaParte DATE, superficie FLOAT(11), cuilEncargado BIGINT(11), nombreEncargado VARCHAR(30), apellidoEncargado VARCHAR(30), cuilEmpleado BIGINT(11), nombreEmpleado VARCHAR(30), apellidoEmpleado VARCHAR(30), rol CHAR(1), OUT mensaje VARCHAR(100))
-SALIR: BEGIN
-			IF 	(nombreFinca IS NULL) OR (fechaParte IS NULL) OR (superficie IS NULL) OR 
-				(cuilEncargado IS NULL) OR (nombreEncargado IS NULL) OR (apellidoEncargado IS NULL)  OR 
-				(cuilEmpleado IS NULL) OR (nombreEmpleado IS NULL) OR (apellidoEmpleado IS NULL)
-			THEN
-				SET mensaje = 'Error al cargar el parte, datos incompletos';
-			LEAVE SALIR;
-            ELSEIF EXISTS (SELECT * FROM LINEAS_PARTES JOIN
-END$$
--- Punto 7: Búsqueda de un Parte.
--- Consideraciones: Se recibe un request con los siguientes datos:
--- 		Nombre de la finca
--- 		Cuil del encargado
--- 		Nombre del empleado que oficia como encargado
--- 		Apellido del empleado que oficia como encargado
--- 		Fecha
--- 		Superficie
--- 		Cuil del empleado
--- 		Nombre del empleado 
--- 		Apellido del empleado
--- 		Rol del empleado 
+
+DROP PROCEDURE IF EXISTS `crearPartes`;
+DROP PROCEDURE IF EXISTS `crearLineasPartes`;
 
 DELIMITER $$
+CREATE PROCEDURE `crearPartes` (IN IN_idFINCA INT, IN_fechaParte DATE, IN_superficie FLOAT(11), IN_idENCARGADO INT, IN_estado CHAR(1), OUT mensaje VARCHAR(100))
+SALIR: BEGIN
 
-CREATE PROCEDURE `buscarParte`(
-    IN p_nombreFinca VARCHAR(30),
-    IN p_fechaParte DATE,
-    IN p_superficie FLOAT(11),
-    IN p_cuilEncargado BIGINT(11),
-    IN p_nombreEncargado VARCHAR(30),
-    IN p_apellidoEncargado VARCHAR(30),
-    OUT p_mensaje VARCHAR(100)
-)
-BEGIN
-    DECLARE v_idPARTE INT;
-    DECLARE v_count INT;
+		IF (IN_idFINCA IS NULL) OR (IN_fechaParte IS NULL) OR (IN_superficie IS NULL) OR (IN_idENCARGADO IS NULL)
+		THEN
+			SET mensaje = 'Error al cargar el parte, datos incompletos';
+		LEAVE SALIR;
+        
+        ELSEIF (IN_fechaParte>NOW())
+        THEN 
+			SET mensaje = 'Error al cargar el parte, fecha posterior a la actual';
+		LEAVE SALIR;
+        
+        ELSEIF (IN_estado IS NULL)
+        THEN
+			START TRANSACTION;
+				INSERT INTO `PARTES` (`fechaParte`, `superficie`, `idENCARGADO`, `idFINCA`)
+				VALUES(IN_fechaParte, IN_superficie, IN_idENCARGADO, IN_idFINCA);
+                SET mensaje = 'No se ingresó estado, se guardó el parte con estado por defecto "PENDIENTE"';
+			COMMIT;
+		LEAVE SALIR;
+        
+        ELSE
+			START TRANSACTION;
+				INSERT INTO `PARTES` (`fechaParte`, `estado`, `superficie`, `idENCARGADO`, `idFINCA`)
+                VALUES(IN_fechaParte, IN_estado, IN_superficie, IN_idENCARGADO, IN_idFINCA);
+                SET mensaje = 'Parte cargado con exito';
+			COMMIT;
+            END IF;
+END$$
+DELIMITER ;
 
-    -- Realizar la búsqueda del parte
-    SELECT P.idPARTE
-    INTO v_idPARTE
-    FROM PARTES P
-    JOIN FINCAS F ON P.idFINCA = F.idFINCA
-    JOIN EMPLEADOS E ON P.idENCARGADO = E.idEMPLEADO
-    WHERE F.nombreFinca = p_nombreFinca
-      AND P.fechaParte = p_fechaParte
-      AND P.superficie = p_superficie
-      AND E.cuil = p_cuilEncargado
-      AND E.nombres = p_nombreEncargado
-      AND E.apellidos = p_apellidoEncargado
-    LIMIT 1;
+-- LLAMADAS AL STORE PROCEDURE `crearPartes`
 
-    -- Verificar si se encontró el parte
-    SET v_count = (SELECT COUNT(*) FROM PARTES P
-                   JOIN FINCAS F ON P.idFINCA = F.idFINCA
-                   JOIN EMPLEADOS E ON P.idENCARGADO = E.idEMPLEADO
-                   WHERE F.nombreFinca = p_nombreFinca
-                     AND P.fechaParte = p_fechaParte
-                     AND P.superficie = p_superficie
-                     AND E.cuil = p_cuilEncargado
-                     AND E.nombres = p_nombreEncargado
-                     AND E.apellidos = p_apellidoEncargado);
+-- Error: Datos incompletos (IN_superficie es NULL)
+CALL crearPartes(1, '2024-06-10', NULL, 1, 'P', @mensaje);
+SELECT @mensaje;
 
-    IF v_count = 0 THEN
-        SET p_mensaje = 'No se encontró el parte con los criterios especificados.';
+-- Error: Fecha posterior a la actual
+CALL crearPartes(1, '2025-01-01', 100.0, 1, 'P', @mensaje);
+SELECT @mensaje;
+
+-- Inserción con estado por defecto "PENDIENTE" (IN_estado es NULL)
+CALL crearPartes(1, '2024-06-10', 100.0, 1, NULL, @mensaje);
+SELECT @mensaje;
+SELECT * FROM AUDITORIAS_PARTES;
+
+-- Inserción correcta
+CALL crearPartes(1, '2024-06-10', 100.0, 1, 'C', @mensaje);
+SELECT @mensaje;
+SELECT * FROM AUDITORIAS_PARTES;
+
+DELIMITER $$
+CREATE PROCEDURE `crearLineasPartes` (IN IN_idPARTE INT, IN_idEMPLEADO INT, IN_rol CHAR(1), OUT mensaje VARCHAR(100))
+SALIR: BEGIN
+		IF (IN_idPARTE IS NULL) OR (IN_idEMPLEADO IS NULL) OR (IN_rol IS NULL)
+		THEN
+			SET mensaje = 'Error al cargar esta linea, datos incompletos';
+		LEAVE SALIR;
+        
+		ELSEIF EXISTS (SELECT * FROM PARTES WHERE idPARTE = IN_idPARTE AND idENCARGADO = IN_idEMPLEADO)
+		THEN
+			SET mensaje = 'Error al cargar esta linea, el empleado ingresado ya fue asignado como encargado';
+        LEAVE SALIR;
+        
+        ELSEIF EXISTS (SELECT * FROM LINEAS_PARTES WHERE idPARTE = IN_idPARTE AND idEMPLEADO = IN_idEMPLEADO)
+        THEN 
+			SET mensaje = 'Error al cargar esta linea, el empleado ya fue ingresado';
+		LEAVE SALIR;
+        
+        ELSE 
+			START TRANSACTION;
+				INSERT INTO `LINEAS_PARTES` (`idPARTE`, `idEMPLEADO`, `rol`)
+				VALUES (IN_idPARTE, IN_idEMPLEADO, IN_rol);
+                SET mensaje ='Linea cargada con exito';
+			COMMIT;
+		END IF;
+END $$
+ DELIMITER ;
+
+-- LLAMADAS AL STORE PROCEDURE `crearLineasPartes`
+
+-- Error: Datos incompletos (IN_idEMPLEADO es NULL)
+CALL crearLineasPartes(103, NULL, 'E', @mensaje);
+SELECT @mensaje;
+
+-- Error: Empleado asignado como encargado en el parte
+CALL crearLineasPartes(103, 1, 'E', @mensaje);
+SELECT @mensaje;
+
+-- Error: Empleado cargado 2 veces
+CALL crearLineasPartes(103, 2, 'E', @mensaje);
+CALL crearLineasPartes(103, 2, 'O', @mensaje);
+SELECT @mensaje;
+
+-- Inserción correcta
+CALL crearLineasPartes(103, 3, 'O', @mensaje);
+SELECT @mensaje;
+SELECT * FROM AUDITORIAS_LINEAS_PARTES;
+
+-- Punto 5: Modificación de un Parte y sus Líneas.
+
+DROP PROCEDURE IF EXISTS `modificarPartes`;
+DROP PROCEDURE IF EXISTS `modificarLineasPartes`;
+
+DELIMITER $$
+CREATE PROCEDURE `modificarPartes` (IN IN_idPARTE INT, IN_idFINCA INT, IN_fechaParte DATE, IN_superficie FLOAT(11), IN_idENCARGADO INT, IN_estado CHAR(1), OUT mensaje VARCHAR(100))
+SALIR: BEGIN
+			IF NOT EXISTS (SELECT * FROM PARTES WHERE idPARTE = IN_idPARTE)  
+            THEN 
+				SET mensaje = 'Error, el parte no existe';
+			LEAVE SALIR;
+            
+            ELSEIF (IN_fechaParte>NOW())
+            THEN 
+				SET mensaje = 'Error al modificar el parte, fecha posterior a la actual';
+			LEAVE SALIR;
+            
+            ELSEIF (IN_idPARTE IS NULL) OR (IN_idFINCA IS NULL) OR (IN_fechaParte IS NULL) OR (IN_superficie IS NULL) OR (idENCARGADO IS NULL)
+			THEN
+				SET mensaje = 'Error al modificar el parte, datos incompletos';
+			LEAVE SALIR; 
+            
+            ELSEIF EXISTS (SELECT * FROM LINEAS_PARTES WHERE idPARTE = IN_idPARTE AND idEMPLEADO = IN_idENCARGADO)
+            THEN 
+				SET mensaje = 'Error al modificar el parte, el empleado ingresado ya fue cargado';
+			LEAVE SALIR; 
+            
+            ELSE
+				-- Actualizar la parte con los nuevos valores o mantener los existentes si los nuevos son nulos
+				UPDATE PARTES
+				SET 
+					idFINCA = IN_idFINCA,
+					fechaParte = IN_fechaParte,
+					superficie = IN_superficie,
+					idENCARGADO = idENCARGADO,
+					estado = IN_estado
+				WHERE idPARTE = IN_idPARTE;
+				SET mensaje = 'Parte modificado exitosamente';
+            END IF;
+END$$
+DELIMITER ;
+
+-- LLAMADAS AL STORE PROCEDURE `modificarPartes`
+
+-- Error: El parte no existe (suponiendo que el idPARTE 999 no existe)
+CALL modificarPartes(999, 1, '2024-06-09', 100.0, 1, 'C', @mensaje);
+SELECT @mensaje;
+
+-- Error: Fecha posterior a la actual
+CALL modificarPartes(103, 1, '2025-01-01', 100.0, 1, 'C', @mensaje);
+SELECT @mensaje;
+
+-- Error: Datos incompletos (IN_superficie es NULL)
+CALL modificarPartes(1, 1, '2024-06-10', NULL, 1, 'C', @mensaje);
+SELECT @mensaje;
+
+-- Modificación correcta
+CALL modificarPartes(1, 1, '2024-06-10', 100.0, 1, 'C', @mensaje);
+SELECT @mensaje;
+SELECT * FROM AUDITORIAS_PARTES;
+
+DELIMITER $$
+CREATE PROCEDURE `modificarLineasPartes` (IN IN_idPARTE INT, IN IN_idEMPLEADO INT, IN IN_rol CHAR(1), OUT mensaje VARCHAR(100))
+SALIR: BEGIN
+    -- Verificar si los datos están completos
+    IF (IN_idPARTE IS NULL) OR (IN_idEMPLEADO IS NULL) OR (IN_rol IS NULL) 
+    THEN
+        SET mensaje = 'Error, datos incompletos';
+        LEAVE SALIR;
+    ELSEIF NOT EXISTS (SELECT * FROM LINEAS_PARTES WHERE idPARTE = IN_idPARTE AND idEMPLEADO = IN_idEMPLEADO) 
+    THEN 
+        SET mensaje = 'Error, La línea ingresada no existe';
+        LEAVE SALIR;
     ELSE
-        SET p_mensaje = CONCAT('Parte encontrado: ID = ', v_idPARTE);
+        -- Actualizar la línea
+        UPDATE LINEAS_PARTES
+        SET rol = IN_rol
+        WHERE idPARTE = IN_idPARTE AND idEMPLEADO = IN_idEMPLEADO;
+
+        SET mensaje = 'Línea modificada exitosamente';
     END IF;
 END $$
-
 DELIMITER ;
+
+ 
+ -- LLAMADAS AL STORE PROCEDURE `modificarLineasPartes`
+ 
+ -- Error: Datos incompletos (IN_idEMPLEADO es NULL)
+CALL modificarLineasPartes(103, NULL, 'E', @mensaje);
+SELECT @mensaje;
+
+-- Error: Error, La linea ingresada no existe
+CALL modificarLineasPartes(103, 144, 'E', @mensaje);
+SELECT @mensaje;
+
+-- Modificación correcta
+CALL modificarLineasPartes(103, 3, 'O', @mensaje);
+SELECT @mensaje;
+SELECT * FROM AUDITORIAS_LINEAS_PARTES;
+
+
+-- Punto 7: Búsqueda de un Parte.
+-- La búsqueda se realiza por la combinación de idFINCA, idENCARGADO, y fecha, para encontrar un único parte
+DROP PROCEDURE IF EXISTS `buscarParte`;
+DELIMITER $$
+CREATE PROCEDURE `buscarParte`(IN IN_idFINCA INT, IN_idENCARGADO INT, IN_fechaParte DATE, OUT mensaje VARCHAR(100)) 
+SALIR: BEGIN
+	IF (IN_idFINCA IS NULL) OR (IN_idENCARGADO IS NULL) OR (IN_fechaParte IS NULL)
+	THEN
+		SET mensaje = 'Error, no se proporcionaron suficientes parámetros';
+		LEAVE SALIR;
+	ELSEIF (IN_fechaParte>NOW())
+    THEN
+		SET mensaje = 'Error, la fecha ingresada es posterior a la fecha atual';
+		LEAVE SALIR;
+	ELSEIF NOT EXISTS (SELECT * FROM PARTES WHERE idFINCA = IN_idFINCA AND idENCARGADO = IN_idENCARGADO AND fechaParte = IN_fechaParte)
+	THEN
+        SET mensaje = 'El parte buscado no existe';
+        LEAVE SALIR;
+	ELSE
+		SELECT F.nombreFinca AS Finca, CONCAT(E.nombres , E.apellidos) AS Encargado, P.fechaParte AS Fecha, P.superficie, P.estado
+        FROM PARTES P
+        INNER JOIN EMPLEADOS E
+        ON P.idENCARGADO = E.idEMPLEADO
+        INNER JOIN FINCAS F
+        ON P.idFINCA = F.idFINCA
+		WHERE P.idFINCA = IN_idFINCA AND P.idENCARGADO = IN_idENCARGADO AND P.fechaParte = IN_fechaParte
+        LIMIT 1;
+	END IF;
+END$$
+DELIMITER ;
+
+-- LLAMADAS AL STORE PROCEDURE `buscarParte
+-- Error: Datos incompletos (IN_idENCARGADO es NULL)
+CALL buscarParte (1, NULL, '2024-06-10', @mensaje);
+SELECT @mensaje;
+
+-- Error: fecha ingresada es posterior a la fecha atual
+CALL buscarParte (1, 2, '2025-06-10', @mensaje);
+SELECT @mensaje;
+
+-- Error: El parte buscado no existe
+CALL buscarParte (1, 2, '2024-06-10', @mensaje);
+SELECT @mensaje;
+
+-- Consulta exitosa
+CALL buscarParte (11, 8, '2024-05-15', @mensaje);
+-- SELECT @mensaje;
